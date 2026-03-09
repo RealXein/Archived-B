d2cb34d477074d3fadc03cd906599156b8729432
RealXein <yassin.xein@gmail.com>
2026-03-09 21:47:23 +0200

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Archived-B/',
@@ -9,4 +8,4 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
   },
-}
+})
