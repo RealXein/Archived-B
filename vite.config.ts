ceb37b138a6b4c88e1cafd67a85d585d3d7f7f19
RealXein <yassin.xein@gmail.com>
2026-03-09 21:52:59 +0200

    Remove build configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Archived-B/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
 })
